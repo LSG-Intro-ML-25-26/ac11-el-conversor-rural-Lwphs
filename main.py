03c13aa451058edc5ed9b764d9edf2f9901e0311
Arnau Garcia <arnau.garcia@gracia.lasalle.cat>
2025-12-18 18:53:07 +0100

    Feat: Basic movement + sprites

--- main.py
+++ main.py
@@ -1,29 +1,10 @@
-def on_up_pressed():
-    animation.run_image_animation(nena,
-        assets.animation("""
-            nena-animation-up
-            """),
-        500,
-        False)
-controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
+@namespace
+class SpriteKind:
+    npc = SpriteKind.create()
 
-def on_left_pressed():
-    animation.run_image_animation(nena,
-        assets.animation("""
-            nena-animation-left
-            """),
-        500,
-        False)
-controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
-
-def on_right_pressed():
-    animation.run_image_animation(nena,
-        assets.animation("""
-            nena-animation-right
-            """),
-        500,
-        False)
-controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
+def on_on_overlap(sprite, otherSprite):
+    pass
+sprites.on_overlap(SpriteKind.player, SpriteKind.npc, on_on_overlap)
 
 def on_down_pressed():
     animation.run_image_animation(nena,
@@ -34,8 +15,55 @@ def on_down_pressed():
         False)
 controller.down.on_event(ControllerButtonEvent.PRESSED, on_down_pressed)
 
+def on_right_pressed():
+    animation.run_image_animation(nena,
+        assets.animation("""
+            nena-animation-right
+            """),
+        500,
+        False)
+controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
+
+def on_left_pressed():
+    animation.run_image_animation(nena,
+        assets.animation("""
+            nena-animation-left
+            """),
+        500,
+        False)
+controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
+
+def on_up_pressed():
+    animation.run_image_animation(nena,
+        assets.animation("""
+            nena-animation-up
+            """),
+        500,
+        False)
+controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
+
 nena: Sprite = None
+scene.set_background_image(assets.image("""
+    Fondillo
+    """))
+npc2 = sprites.create(assets.image("""
+    monillo
+    """), SpriteKind.npc)
+npc2.set_position(27, 99)
 nena = sprites.create(assets.image("""
     nena-front
     """), SpriteKind.player)
+nena.set_position(131, 99)
+nena.set_stay_in_screen(True)
 controller.move_sprite(nena)
+animation.run_image_animation(npc2,
+    assets.animation("""
+        monillo_animacio
+        """),
+    200,
+    True)
+
+def on_forever():
+    if nena.x < 85:
+        npc2.say_text("Hola!", 500)
+forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,15 +1,13 @@
-controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    nena,
-    assets.animation`nena-animation-up`,
-    500,
-    false
-    )
+namespace SpriteKind {
+    export const npc = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.npc, function (sprite, otherSprite) {
+	
 })
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     nena,
-    assets.animation`nena-animation-left`,
+    assets.animation`nena-animation-down`,
     500,
     false
     )
@@ -22,14 +20,38 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     false
     )
 })
-controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     nena,
-    assets.animation`nena-animation-down`,
+    assets.animation`nena-animation-left`,
+    500,
+    false
+    )
+})
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    nena,
+    assets.animation`nena-animation-up`,
     500,
     false
     )
 })
 let nena: Sprite = null
+scene.setBackgroundImage(assets.image`Fondillo`)
+let npc2 = sprites.create(assets.image`monillo`, SpriteKind.npc)
+npc2.setPosition(27, 99)
 nena = sprites.create(assets.image`nena-front`, SpriteKind.Player)
+nena.setPosition(131, 99)
+nena.setStayInScreen(true)
 controller.moveSprite(nena)
+animation.runImageAnimation(
+npc2,
+assets.animation`monillo_animacio`,
+200,
+true
+)
+forever(function () {
+    if (nena.x < 85) {
+        npc2.sayText("Hola!", 500)
+    }
+})
