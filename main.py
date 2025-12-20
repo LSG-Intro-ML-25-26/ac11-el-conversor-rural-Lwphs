ee4595b3fd836dfb97a846d71aaa287b0a024c17
Arnau Garcia <arnau.garcia@gracia.lasalle.cat>
2025-12-20 23:57:11 +0100

    Feat: Comprar items a canvi de llenya

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 class SpriteKind:
     npc = SpriteKind.create()
     arbre = SpriteKind.create()
+
+def on_on_overlap(sprite, otherSprite):
+    nena.say_text("A per vendre", 200)
+sprites.on_overlap(SpriteKind.player, SpriteKind.npc, on_on_overlap)
+
 def crearArbres():
     global numero_arboles, arbol2, posicion_x, posicion_y, index
     numero_arboles = randint(20, 25)
@@ -43,7 +48,7 @@ controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
 
 def on_a_pressed():
     global arbol_tocado, seleccio_menu, myMenu
-    arbol_tocado = None
+    arbol_tocado = None  
     for arbol in sprites.all_of_kind(SpriteKind.arbre):
         if nena.overlaps_with(arbol):
             arbol_tocado = arbol
@@ -51,26 +56,72 @@ def on_a_pressed():
     if arbol_tocado:
         sprites.destroy(arbol_tocado)
         nena.say_text("He recollit llenya!", 1000)
+        arbol_tocado = None 
     elif nena.overlaps_with(npc2) and seleccio_menu == 0:
         seleccio_menu = 1
         controller.move_sprite(nena, 0, 0)
-        
-        myMenu = miniMenu.create_menu(miniMenu.create_menu_item("Gallina"),
-            miniMenu.create_menu_item("Gallina"),
-            miniMenu.create_menu_item("Patates"),
-            miniMenu.create_menu_item("Dotzena"),
-            miniMenu.create_menu_item("Caball"),
+        myMenu = miniMenu.create_menu(miniMenu.create_menu_item("Cabra (5 llenya)"),
+            miniMenu.create_menu_item("Gallina (6 llenya)"),
+            miniMenu.create_menu_item("Patates (2 llenya)"),
+            miniMenu.create_menu_item("Dotzena d'ous (3 llenya)"),
+            miniMenu.create_menu_item("Caball (12 llenya)"),
             miniMenu.create_menu_item("Tancar menú"))
-        
         myMenu.set_title("mercat de trueque!")
+        myMenu.on_button_pressed(controller.A, on_menu_selection)
 controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
 
 def on_b_pressed():
     global seleccio_menu
-    myMenu.close()
-    controller.move_sprite(nena, 100, 100)
-    seleccio_menu = 0
+    if seleccio_menu > 0:
+        if myMenu:
+            myMenu.close()
+        controller.move_sprite(nena, 100, 100)
+        seleccio_menu = 0
 controller.B.on_event(ControllerButtonEvent.PRESSED, on_b_pressed)
+
+def mostrar_menu_cantidad(nombre: str, precio: number):
+    global item_nombre_temp, item_precio_temp, seleccio_menu, myMenu
+    item_nombre_temp = nombre
+    item_precio_temp = precio
+    seleccio_menu = 2
+    myMenu = miniMenu.create_menu(miniMenu.create_menu_item("1"),
+        miniMenu.create_menu_item("2"),
+        miniMenu.create_menu_item("3"),
+        miniMenu.create_menu_item("4"),
+        miniMenu.create_menu_item("5"),
+        miniMenu.create_menu_item("Tornar enrere"))
+    myMenu.set_title("Quants/es " + nombre + "s?")
+    
+    def on_button_pressed(selection, selectedIndex):
+        global seleccio_menu, myMenu, llenya_actual
+        if myMenu:
+            myMenu.close()
+        if selectedIndex == 5:
+            seleccio_menu = 1
+            myMenu = miniMenu.create_menu(miniMenu.create_menu_item("Cabra (5 llenya)"),
+                miniMenu.create_menu_item("Gallina (6 llenya)"),
+                miniMenu.create_menu_item("Patates (2 llenya)"),
+                miniMenu.create_menu_item("Dotzena d'ous (3 llenya)"),
+                miniMenu.create_menu_item("Caball (12 llenya)"),
+                miniMenu.create_menu_item("Tancar menú"))
+            myMenu.set_title("mercat de trueque!")
+            myMenu.on_button_pressed(controller.A, on_menu_selection)
+        else:
+            cantidad = selectedIndex + 1
+            costo_total = item_precio_temp * cantidad
+            if llenya_actual >= costo_total:
+                llenya_actual += 0 - costo_total
+                info.set_score(llenya_actual)
+                game.splash("Has comprat " + ("" + str(cantidad)) + " " + item_nombre_temp)
+                game.splash("Cost: " + ("" + str(costo_total)) + " llenya")
+                controller.move_sprite(nena, 100, 100)
+                seleccio_menu = 0
+            else:
+                controller.move_sprite(nena, 100, 100)
+                seleccio_menu = 0
+                npc2.say_text("Necessites " + ("" + str(costo_total)) + " llenya!", 3000)
+    myMenu.on_button_pressed(controller.A, on_button_pressed)
+    
 
 def on_up_pressed():
     animation.run_image_animation(nena,
@@ -81,21 +132,45 @@ def on_up_pressed():
         False)
 controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
 
-def on_on_destroyed(sprite):
-    info.change_score_by(1)
+def on_menu_selection(selection2: str, selectedIndex2: number):
+    global seleccio_menu
+    if myMenu:
+        myMenu.close()
+    if selectedIndex2 == 5:
+        controller.move_sprite(nena, 100, 100)
+        seleccio_menu = 0
+    elif selectedIndex2 == 0:
+        mostrar_menu_cantidad("Cabra", 5)
+    elif selectedIndex2 == 1:
+        mostrar_menu_cantidad("Gallina", 6)
+    elif selectedIndex2 == 2:
+        mostrar_menu_cantidad("Patates", 2)
+    elif selectedIndex2 == 3:
+        mostrar_menu_cantidad("Dotzena d'ous", 3)
+    elif selectedIndex2 == 4:
+        mostrar_menu_cantidad("Caball", 12)
+
+def on_on_destroyed(sprite2):
+    global llenya_actual
+    llenya_actual += 3
+    info.change_score_by(3)
 sprites.on_destroyed(SpriteKind.arbre, on_on_destroyed)
 
+ha_saludat = False
+llenya_actual = 0
+item_precio_temp = 0
+item_nombre_temp = ""
 seleccio_menu = 0
+arbol_tocado: Sprite = None
 posicion_y = 0
 posicion_x = 0
 arbol2: Sprite = None
 index = 0
 numero_arboles = 0
-myMenu: miniMenu.MenuSprite = None
 nena: Sprite = None
 npc2: Sprite = None
-seleccio_menu2 = 0
-arbol_tocado: Sprite = None
+myMenu: miniMenu.MenuSprite = None
+pot_vendre = False
 scene.set_background_image(assets.image("""
     Fondillo
     """))
@@ -121,8 +196,12 @@ myMenu.close()
 info.set_score(0)
 
 def on_forever():
-    if nena.x < 85:
-        npc2.say_text("Hola!", 500)
-    else:
-        myMenu.close()
+    global ha_saludat
+    if nena.x < 85 and not (ha_saludat):
+        npc2.say_text("Hola!", 1000)
+        ha_saludat = True
+    elif nena.x >= 85:
+        ha_saludat = False
+        if myMenu:
+            myMenu.close()
 forever(on_forever)
--- main.ts
+++ main.ts
@@ -3,6 +3,9 @@ namespace SpriteKind {
     export const arbre = SpriteKind.create()
 }
 
+sprites.onOverlap(SpriteKind.Player, SpriteKind.npc, function on_on_overlap(sprite: Sprite, otherSprite: Sprite) {
+    nena.sayText("A per vendre", 200)
+})
 function crearArbres() {
     
     numero_arboles = randint(20, 25)
@@ -45,39 +48,117 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
     if (arbol_tocado) {
         sprites.destroy(arbol_tocado)
         nena.sayText("He recollit llenya!", 1000)
+        arbol_tocado = null
     } else if (nena.overlapsWith(npc2) && seleccio_menu == 0) {
         seleccio_menu = 1
         controller.moveSprite(nena, 0, 0)
-        myMenu = miniMenu.createMenu(miniMenu.createMenuItem("Gallina"), miniMenu.createMenuItem("Gallina"), miniMenu.createMenuItem("Patates"), miniMenu.createMenuItem("Dotzena"), miniMenu.createMenuItem("Caball"), miniMenu.createMenuItem("Tancar menú"))
+        myMenu = miniMenu.createMenu(miniMenu.createMenuItem("Cabra (5 llenya)"), miniMenu.createMenuItem("Gallina (6 llenya)"), miniMenu.createMenuItem("Patates (2 llenya)"), miniMenu.createMenuItem("Dotzena d'ous (3 llenya)"), miniMenu.createMenuItem("Caball (12 llenya)"), miniMenu.createMenuItem("Tancar menú"))
         myMenu.setTitle("mercat de trueque!")
+        myMenu.onButtonPressed(controller.A, on_menu_selection)
     }
     
 })
 controller.B.onEvent(ControllerButtonEvent.Pressed, function on_b_pressed() {
     
-    myMenu.close()
-    controller.moveSprite(nena, 100, 100)
-    seleccio_menu = 0
+    if (seleccio_menu > 0) {
+        if (myMenu) {
+            myMenu.close()
+        }
+        
+        controller.moveSprite(nena, 100, 100)
+        seleccio_menu = 0
+    }
+    
 })
+function mostrar_menu_cantidad(nombre: string, precio: number) {
+    
+    item_nombre_temp = nombre
+    item_precio_temp = precio
+    seleccio_menu = 2
+    myMenu = miniMenu.createMenu(miniMenu.createMenuItem("1"), miniMenu.createMenuItem("2"), miniMenu.createMenuItem("3"), miniMenu.createMenuItem("4"), miniMenu.createMenuItem("5"), miniMenu.createMenuItem("Tornar enrere"))
+    myMenu.setTitle("Quants/es " + nombre + "s?")
+    myMenu.onButtonPressed(controller.A, function on_button_pressed(selection: any, selectedIndex: any) {
+        let cantidad: number;
+        let costo_total: number;
+        
+        if (myMenu) {
+            myMenu.close()
+        }
+        
+        if (selectedIndex == 5) {
+            seleccio_menu = 1
+            myMenu = miniMenu.createMenu(miniMenu.createMenuItem("Cabra (5 llenya)"), miniMenu.createMenuItem("Gallina (6 llenya)"), miniMenu.createMenuItem("Patates (2 llenya)"), miniMenu.createMenuItem("Dotzena d'ous (3 llenya)"), miniMenu.createMenuItem("Caball (12 llenya)"), miniMenu.createMenuItem("Tancar menú"))
+            myMenu.setTitle("mercat de trueque!")
+            myMenu.onButtonPressed(controller.A, on_menu_selection)
+        } else {
+            cantidad = selectedIndex + 1
+            costo_total = item_precio_temp * cantidad
+            if (llenya_actual >= costo_total) {
+                llenya_actual += 0 - costo_total
+                info.setScore(llenya_actual)
+                game.splash("Has comprat " + ("" + ("" + cantidad)) + " " + item_nombre_temp)
+                game.splash("Cost: " + ("" + ("" + costo_total)) + " llenya")
+                controller.moveSprite(nena, 100, 100)
+                seleccio_menu = 0
+            } else {
+                controller.moveSprite(nena, 100, 100)
+                seleccio_menu = 0
+                npc2.sayText("Necessites " + ("" + ("" + costo_total)) + " llenya!", 3000)
+            }
+            
+        }
+        
+    })
+}
+
 controller.up.onEvent(ControllerButtonEvent.Pressed, function on_up_pressed() {
     animation.runImageAnimation(nena, assets.animation`
             nena-animation-up
             `, 500, false)
 })
-sprites.onDestroyed(SpriteKind.arbre, function on_on_destroyed(sprite: Sprite) {
-    info.changeScoreBy(1)
+function on_menu_selection(selection2: string, selectedIndex2: number) {
+    
+    if (myMenu) {
+        myMenu.close()
+    }
+    
+    if (selectedIndex2 == 5) {
+        controller.moveSprite(nena, 100, 100)
+        seleccio_menu = 0
+    } else if (selectedIndex2 == 0) {
+        mostrar_menu_cantidad("Cabra", 5)
+    } else if (selectedIndex2 == 1) {
+        mostrar_menu_cantidad("Gallina", 6)
+    } else if (selectedIndex2 == 2) {
+        mostrar_menu_cantidad("Patates", 2)
+    } else if (selectedIndex2 == 3) {
+        mostrar_menu_cantidad("Dotzena d'ous", 3)
+    } else if (selectedIndex2 == 4) {
+        mostrar_menu_cantidad("Caball", 12)
+    }
+    
+}
+
+sprites.onDestroyed(SpriteKind.arbre, function on_on_destroyed(sprite2: Sprite) {
+    
+    llenya_actual += 3
+    info.changeScoreBy(3)
 })
+let ha_saludat = false
+let llenya_actual = 0
+let item_precio_temp = 0
+let item_nombre_temp = ""
 let seleccio_menu = 0
+let arbol_tocado : Sprite = null
 let posicion_y = 0
 let posicion_x = 0
 let arbol2 : Sprite = null
 let index = 0
 let numero_arboles = 0
-let myMenu : miniMenu.MenuSprite = null
 let nena : Sprite = null
 let npc2 : Sprite = null
-let seleccio_menu2 = 0
-let arbol_tocado : Sprite = null
+let myMenu : miniMenu.MenuSprite = null
+let pot_vendre = false
 scene.setBackgroundImage(assets.image`
     Fondillo
     `)
@@ -99,10 +180,16 @@ myMenu = miniMenu.createMenu(miniMenu.createMenuItem("abc"))
 myMenu.close()
 info.setScore(0)
 forever(function on_forever() {
-    if (nena.x < 85) {
-        npc2.sayText("Hola!", 500)
-    } else {
-        myMenu.close()
+    
+    if (nena.x < 85 && !ha_saludat) {
+        npc2.sayText("Hola!", 1000)
+        ha_saludat = true
+    } else if (nena.x >= 85) {
+        ha_saludat = false
+        if (myMenu) {
+            myMenu.close()
+        }
+        
     }
     
 })
